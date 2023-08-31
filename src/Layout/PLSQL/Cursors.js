5f508f08d73b7c3171995158a96512197c234033
sharad-beep <70638080+sharad-beep@users.noreply.github.com>
2023-09-01 01:00:22 +0530

    rushi added

--- src/Layout/PLSQL/Cursors.js
+++ src/Layout/PLSQL/Cursors.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PlsqlMenu from './PlsqlMenu'
 import Layout from '../Layout'
+import one from "./plsql_cur_1.PNG";
+
 const Cursors = () => {
   return (
 	<div>
@@ -12,7 +14,131 @@ const Cursors = () => {
 			  </div>
 			  <div className="col-md-9">
 			 
-				 <h1>Curosors info</h1>
+				 <h1>PL/SQL Curosors</h1>
+				 <p>
+				 PL/SQL cursors are database objects used to process and manipulate the result sets returned by a SELECT statement. Cursors provide a way to retrieve and work with query results row by row, enabling developers to perform various operations on the retrieved data. Cursors are particularly useful when dealing with large datasets or when performing operations that involve multiple interactions with the database.
+<br></br>
+Types of Cursors:<br></br>
+<br></br>
+
+
+Implicit Cursors: Automatically created by the database for every DML statement (INSERT, UPDATE, DELETE) and SELECT INTO statement. These cursors manage query results without explicit user intervention.
+<br></br>
+
+Explicit Cursors: Created and managed by developers explicitly to handle query results in a more controlled manner.
+<br></br>
+
+Explicit Cursor Syntax:
+<br></br>
+<div>
+  <img src={one} alt="Image of plsql_cur_1" />
+</div>
+<br></br>
+
+
+
+Explanation:<br></br>
+<br></br>
+
+
+DECLARE: Marks the beginning of the PL/SQL block.<br></br>
+
+cursor_name: Name of the explicit cursor.<br></br>
+
+SELECT columns FROM table WHERE conditions: The SQL query defining the result set.<br></br>
+
+variable_name: Variable used to store data fetched from the cursor.<br></br>
+
+OPEN cursor_name: Opens the cursor, initializing it for data retrieval.<br></br>
+
+LOOP: A loop to iterate through the rows retrieved from the cursor.<br></br>
+
+FETCH cursor_name INTO variable_name: Retrieves data from the cursor and stores it in the variable.<br></br>
+
+EXIT WHEN cursor_name%NOTFOUND: Exits the loop when there are no more rows to fetch.<br></br>
+
+CLOSE cursor_name: Closes the cursor after data processing.<br></br>
+
+EXCEPTION: Handles exceptions raised during cursor processing.<br></br>
+<br></br>
+
+
+Example of an Explicit Cursor:<br></br>
+<br></br>
+
+
+DECLARE<br></br>
+
+    CURSOR employee_cursor IS<br></br>
+
+        SELECT employee_id, first_name, last_name FROM employees WHERE department_id = 30;<br></br>
+		<br></br>
+
+        
+    emp_id employees.employee_id%TYPE;<br></br>
+
+    emp_first_name employees.first_name%TYPE;<br></br>
+
+    emp_last_name employees.last_name%TYPE;<br></br>
+
+BEGIN<br></br>
+
+    OPEN employee_cursor;<br></br>
+
+    LOOP<br></br>
+
+        FETCH employee_cursor INTO emp_id, emp_first_name, emp_last_name;<br></br>
+
+        EXIT WHEN employee_cursor%NOTFOUND;<br></br>
+
+        
+        DBMS_OUTPUT.PUT_LINE('Employee ID: ' || emp_id || ', Name: ' || emp_first_name || ' ' || emp_last_name);<br></br>
+
+    END LOOP;<br></br>
+
+    CLOSE employee_cursor;<br></br>
+
+EXCEPTION<br></br>
+
+    WHEN OTHERS THEN<br></br>
+
+        DBMS_OUTPUT.PUT_LINE('An error occurred.');<br></br>
+
+END;<br></br>
+<br></br>
+
+
+Explanation:<br></br>
+
+In this example, the explicit cursor employee_cursor retrieves employee details from the employees table for the employees in department 30. The data is fetched row by row, and each employee's ID and name are displayed using DBMS_OUTPUT.PUT_LINE.
+<br></br><br></br>
+
+
+Advantages of Cursors:<br></br>
+
+
+Cursors enable processing of query results one row at a time, reducing memory consumption for large datasets.<br></br>
+
+Cursors provide control over query execution and result processing.<br></br>
+
+They can help in performing complex calculations and transformations on the retrieved data.<br></br>
+
+<br></br>
+
+Considerations:<br></br>
+
+Explicit cursors require manual opening, fetching, and closing, which can lead to additional coding and potential errors.<br></br>
+
+Cursors should be closed after use to release resources.<br></br>
+
+<br></br>
+
+Conclusion:<br></br>
+
+PL/SQL cursors, both implicit and explicit, offer a powerful mechanism for working with query results in a controlled and efficient manner. Explicit cursors provide developers with more control over the retrieval and processing of data from query results, making them invaluable tools when dealing with complex operations on database data.
+<br></br>
+
+				 </p>
 				
 			  </div>
 			</div>
--- src/Layout/PLSQL/Exceptions.js
+++ src/Layout/PLSQL/Exceptions.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PlsqlMenu from './PlsqlMenu'
 import Layout from '../Layout'
+import one from "./plsql_exp_1.PNG";
+import two from "./plsql_exp_2.PNG";
 const Exceptions = () => {
   return (
 	<div>
@@ -12,7 +14,59 @@ const Exceptions = () => {
 			  </div>
 			  <div className="col-md-9">
 			 
-				 <h1>Exceptions info</h1>
+				 <h1> PL/SQL Exceptions </h1>
+				 <p>
+
+PL/SQL exceptions are errors or abnormal conditions that can occur during the execution of a PL/SQL program. Exceptions allow developers to handle unexpected situations gracefully, providing a mechanism to capture and manage errors, prevent application crashes, and ensure proper error reporting to users and administrators. PL/SQL provides a structured way to handle exceptions using blocks of code that catch and manage specific error conditions.
+<br></br>
+Types of Exceptions:<br></br><br></br>
+
+Predefined Exceptions: These are standard exceptions provided by PL/SQL, covering common error scenarios. Examples include NO_DATA_FOUND, TOO_MANY_ROWS, and ZERO_DIVIDE.
+<br></br>
+User-Defined Exceptions: Developers can define custom exceptions to handle specific application-related errors.
+<br></br>
+Exception Handling Syntax:
+<br></br>
+<div>
+  <img src={one} alt="Image of plsql_exp_1" />
+</div>
+
+Explanation:<br></br>
+<br></br>
+DECLARE: Marks the beginning of the PL/SQL block.<br></br>
+exception_name1, exception_name2, etc.: The predefined or user-defined exceptions being caught.<br></br>
+Code to handle exception: Code block to handle the specific exception.<br></br>
+EXCEPTION: Marks the beginning of the exception handling block.<br></br>
+WHEN exception_name: Specifies which exception is being handled.<br></br>
+Additional exception handlers: More handlers can be added to handle different exceptions.<br></br>
+<br></br>
+Example of Exception Handling:<br></br>
+
+<div>
+  <img src={two} alt="Image of plsql_exp_2" />
+</div>
+
+
+
+
+
+Explanation:<br></br>
+In this example, an error occurs when the balance goes negative. The VALUE_ERROR exception is raised explicitly. The code block under WHEN VALUE_ERROR handles the error by displaying an error message. If any other exception occurs (WHEN OTHERS), a generic error message is displayed.
+<br></br>
+Advantages of Exception Handling:<br></br>
+<br></br>
+Graceful Error Handling: Exceptions provide a structured way to handle errors, ensuring that the program doesn't crash due to unexpected situations.<br></br>
+Error Reporting: Exception messages can be tailored to provide meaningful information to users and administrators.<br></br>
+Modularization: Exception handling code can be centralized, making it easier to manage and update.<br></br><br></br>
+Considerations:<br></br>
+<br></br>
+Overusing exceptions can make code harder to understand and maintain.<br></br>
+Exception handlers should not hide underlying issues but rather provide informative error messages.<br></br><br></br>
+Conclusion:<br></br>
+
+Exception handling is a crucial aspect of writing robust and reliable PL/SQL programs. By anticipating and managing errors effectively, developers can ensure that their applications respond gracefully to unexpected situations, enhancing user experience and maintaining data integrity. Properly designed exception handling helps in troubleshooting, debugging, and maintaining the stability of database applications.
+<br></br>
+				 </p>
 				
 			  </div>
 			</div>
